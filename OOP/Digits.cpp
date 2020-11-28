97ea12b801495678f533e424cc37e6276095b645
Nishil <63183230+Nishil07@users.noreply.github.com>
2020-11-28 12:11:16 +0530

    Create Digits.cpp

---
+++ OOP/Digits.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class digits{
+    public:
+        int sum1,r,sum2=0;
+
+    int sum(int a)
+    {
+        sum1=0;
+        r=0;
+        if(a<10)
+        {
+            return a;
+        }
+        while(a!=0)
+        {
+            r=a%10;
+            sum1+=r;
+            a=a/10;
+        }
+        if(sum1>=10)
+        {
+            a=sum1;
+            sum1=sum(a);
+        }
+        return sum1;
+        
+    }
+
+    int sum(string h)
+    {
+        int i,a=0;
+        for(i=0;h[i]!='\0';i++)
+        {
+            a+=(int)h[i];
+        }
+        return sum(a);
+    }
+};
+
+int main()
+{
+    digits obj;
+    int x;
+    string y;
+    cout<<"Digit:"<<endl;
+    cin>>x;
+    cout<<"String:"<<endl;
+    cin>>y;
+    cout<<"The single digit value is:"<<obj.sum(x)<<endl;
+    cout<<"The single digit value for the string is:"<<obj.sum(y)<<endl;
+}
