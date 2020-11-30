696e437599eba1a645a3ce53b3e76bb65e3708ba
Nishil <63183230+Nishil07@users.noreply.github.com>
2020-11-30 23:59:02 +0530

    Create virtual_function.cpp

---
+++ OOP/virtual_function.cpp
@@ -0,0 +1,7 @@
+Create a class telephone_directory (name, occupation, phone, address) and an virtual
+function search (), that searches the object contents in a linear fashion. Extend the class in a
+specific class called doctor (qualification, clinic, visiting time) and override the virtual function
+such that it searches using binary search. Also create another class lawyer (qualification,
+civil/criminal, cases attended, contact_no, office_address) that extends the telephone_directory
+such that it won’t override the virtual function. Search for a doctor and a lawyer by taking the
+option from the user. 
